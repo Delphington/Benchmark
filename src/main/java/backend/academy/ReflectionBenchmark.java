f1598d96756d7aaa292c3c5eacb3052e12339d61
Delphington <Delphington@gmail.com>
2024-12-17 11:44:55 +0300

    init

---
+++ src/main/java/backend/academy/ReflectionBenchmark.java
@@ -0,0 +1,51 @@
+package backend.academy;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import java.lang.invoke.CallSite;
+import java.lang.invoke.LambdaMetafactory;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+@State(Scope.Thread)
+public class ReflectionBenchmark {
+    private Student student;
+    private Method method;
+    private MethodHandle methodHandle;
+    private Function<Student, String> function;
+    private MethodHandles.Lookup lookup;
+    private CallSite callSite;
+
+    public static void main(String[] args) throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(ReflectionBenchmark.class.getSimpleName())
+            .shouldFailOnError(true)
+            .shouldDoGC(true)
+            .mode(Mode.AverageTime)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .forks(1)
+            .warmupForks(1)
+            .warmupIterations(1)
+            .warmupTime(TimeValue.seconds(5))
+            .measurementIterations(1)
+            .measurementTime(TimeValue.seconds(5))
+            .build();
+        new Runner(options).run();
+    }
+
+
+}
